12b59095e9c8eb589adad5f956433d1b5f17844e
zhengxing <970389745@qq.com>
2017-06-22 18:11:55 +0800

    pattern

--- src/main/singleton/com/zx/staticclass/StaticClassClass.java
+++ src/main/singleton/com/zx/staticclass/StaticClassClass.java
@@ -4,4 +4,14 @@ package com.zx.staticclass;
  * 静态内部类模式
  */
 public class StaticClassClass {
+    //私有化构造方法
+    private StaticClassClass(){}
+    //静态内部类
+    private static class StaticClassClassInternal{
+        private static StaticClassClass instance = new StaticClassClass();
+    }
+    //获取实例
+    public static StaticClassClass getInstance(){
+        return StaticClassClassInternal.instance;
+    }
 }
